9eb26894ea688a260effe1e9d7fb7bea0e94714d
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-04-17 01:25:35 -0600

    esto si

--- src/app/finance/page.tsx
+++ src/app/finance/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { classNames } from '@/utilities/classNames'
 import {
